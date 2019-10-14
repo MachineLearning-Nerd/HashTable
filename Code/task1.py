225e1758245b4c4921ceceb23c9425cce886c2b6
Dinesh <jinjaladinesh@gmail.com>
2019-10-14 18:44:41 +0530

    Linear Hash

--- Code/task1.py
+++ Code/task1.py
@@ -6,31 +6,56 @@
 class HashTable:
     def __init__(self, table_capacity=1024, hash_base=17): 
         self.table = [None] * table_capacity
+        self.max_length = table_capacity
         self.base = hash_base
         self.count = 0
   
     def __getitem__(self, key):
-        key_hash = self.hash(key)
-        if self.table[key_hash] is not None:
-            for pair in self.table[key_hash]:
-                if pair[0] == key:
-                    return pair[1]
-        else :
-            raise  KeyError()
+        # key_hash = self.hash(key)
+        # if self.table[key_hash] is not None:
+        #     for pair in self.table[key_hash]:
+        #         if pair[0] == key:
+        #             return pair[1]
+        # else :
+        #     raise  KeyError()
+        hashed_key = self.hash(key)
+        if self.table[hashed_key] is None:
+            raise KeyError
+        if self.table[hashed_key][0] != key:
+            original_key = hashed_key
+            while self.table[hashed_key][0] != key:
+                hashed_key = (hashed_key + 1) % self.max_length # self._increment_key(hashed_key)
+                if self.table[hashed_key] is None:
+                    raise KeyError
+                if hashed_key == original_key:
+                    raise KeyError
+        
+        return self.table[hashed_key][1]
 
     def __setitem__(self, key, item):
-        key_hash = self.hash(key)
-        key_value = [key, item] if item != None else [key, str(item)]
-        if self.table[key_hash] is None:
-            self.table[key_hash] = list([key_value])
-            return True
-        else:
-            for pair in self.table[key_hash]:
-                if pair[0] == key:
-                    pair[1] =  item
-                    return True
-            self.table[key_hash].append(key_value)
-            return True
+        # key_hash = self.hash(key)
+        # key_value = [key, item] if item != None else [key, str(item)]
+        # if self.table[key_hash] is None:
+        #     self.table[key_hash] = list([key_value])
+        #     return True
+        # else:
+        #     for pair in self.table[key_hash]:
+        #         if pair[0] == key:
+        #             pair[1] =  item
+        #             return True
+        #     self.table[key_hash].append(key_value)
+        #     return True
+        self.count += 1
+        hashed_key = self.hash(key)
+        while self.table[hashed_key] is not None:
+            if self.table[hashed_key][0] == key:
+                self.count -= 1
+                break
+            hashed_key = (hashed_key + 1) % self.max_length #self._increment_key(hashed_key)
+        keytuple = (key, item)
+        self.table[hashed_key] = keytuple
+        if self.length / float(self.max_length) >= 1:
+            self.rehash()
 
     def __contains__(self, key):
         try:
