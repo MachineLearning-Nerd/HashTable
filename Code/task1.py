8d2e7919173f0e40fc5e8793d79b3433436e88e8
Dinesh <jinjaladinesh@gmail.com>
2019-10-14 20:24:22 +0530

    New Hash table method

--- Code/task1.py
+++ Code/task1.py
@@ -4,62 +4,65 @@
 """
 
 class HashTable:
+    # This is the __init__ function
     def __init__(self, table_capacity=1024, hash_base=17): 
+        # It have default lengh of table_capacity and hash_bash
         self.table = [None] * table_capacity
+        # This variable shows the maximum length
         self.max_length = table_capacity
         self.base = hash_base
         self.count = 0
   
+    # This function is the called when we have something like
+    # x['abc']
     def __getitem__(self, key):
-        # key_hash = self.hash(key)
-        # if self.table[key_hash] is not None:
-        #     for pair in self.table[key_hash]:
-        #         if pair[0] == key:
-        #             return pair[1]
-        # else :
-        #     raise  KeyError()
+        # This hash function will find the hash key
         hashed_key = self.hash(key)
         if self.table[hashed_key] is None:
+            # if key have None value then return Exception
             raise KeyError
         if self.table[hashed_key][0] != key:
+            # If the key is not at the hashed_key
             original_key = hashed_key
+            # This is done because we have used the linear Probing 
             while self.table[hashed_key][0] != key:
-                hashed_key = (hashed_key + 1) % self.max_length # self._increment_key(hashed_key)
+                # Find out the new hashed_key for linear probing
+                hashed_key = (hashed_key + 1) % self.max_length 
                 if self.table[hashed_key] is None:
                     raise KeyError
                 if hashed_key == original_key:
                     raise KeyError
-        
         return self.table[hashed_key][1]
 
+    # This function will be called when we have something like
+    # x['abc'] = 5
+    # So this function will set the value at given key
     def __setitem__(self, key, item):
-        # key_hash = self.hash(key)
-        # key_value = [key, item] if item != None else [key, str(item)]
-        # if self.table[key_hash] is None:
-        #     self.table[key_hash] = list([key_value])
-        #     return True
-        # else:
-        #     for pair in self.table[key_hash]:
-        #         if pair[0] == key:
-        #             pair[1] =  item
-        #             return True
-        #     self.table[key_hash].append(key_value)
-        #     return True
+        # Count defines the number of keys have been uploaded to the Hash Table
         self.count += 1
+        # Find out the Hash key
         hashed_key = self.hash(key)
         while self.table[hashed_key] is not None:
             if self.table[hashed_key][0] == key:
                 self.count -= 1
                 break
-            hashed_key = (hashed_key + 1) % self.max_length #self._increment_key(hashed_key)
+            hashed_key = (hashed_key + 1) % self.max_length 
+        # Store the key and the item in tuple format
         keytuple = (key, item)
         self.table[hashed_key] = keytuple
-        if self.length / float(self.max_length) >= 1:
+        # If the lsit is full then go to this function
+        if self.count / float(self.max_length) >= 1:
             self.rehash()
 
+    # This function will be called when we have something like 
+    # 'abc' in x
+    # This function will return True or False
     def __contains__(self, key):
+        # If the Hash table have that key then it will return True
         try:
-            return self.__getitem__(key) != None
+            self.__getitem__(key)
+            return True 
+        # Other wise it will return False
         except Exception as e:
             return False
 
@@ -78,5 +81,34 @@ class HashTable:
         # Return the index within the hash table size by modulo operator
         return hashsum 
 
+    # This function will be called when Hash table is full and 
+    # We want to insert the data
     def rehash(self):
-        raise NotImplementedError
+        # This is the list of the prime number given in the assignment
+        Primes = [ 3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761,
+                    919, 1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
+                    17519, 21023, 25229, 30313, 36353, 43627, 52361, 62851, 75521, 90523, 108631, 130363, 156437,
+                    187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
+                    1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369]
+        # Find out the prime number which is larger then the double
+        # of the length of the current table
+        nlength = 0 
+        for val in Primes:
+            if val < 2 * self.max_length:
+                continue
+            else: 
+                nlength = val
+                break
+        # Raise the value error when the newlenght is not in the prime number list
+        if nlength == 0:
+            raise ValueError
+
+        self.max_length = nlength
+        self.count = 0
+        # Store old table
+        old_table = self.table
+        self.table = [None] * self.max_length
+        # Create new table with increased size
+        for keytuple in old_table:
+            if keytuple is not None:
+                self[keytuple[0]] = keytuple[1]
--- Code/test_task1.py
+++ Code/test_task1.py
@@ -39,14 +39,18 @@ class TestTask1(TestCase):
       x["definitely a string"] = None
       x["abdcef"] = "abcdef"
     
+    # Check for the Ture condition
     for key in ["abcdef", "definitely a string", "abdcef"]:
       with self.vis():
         self.assertTrue(key in x, "False negative in __contains__ for key {}".format(key))
 
+    # Check False condition
+    with self.vis():
+      self.assertEqual("key" in x, False, msg = "contains failed.")
+
     assert self.check_okay("contains")
 
   def test_getitem(self):
-    print("This is the task ")
     x = task1.HashTable(1024, 1)
 
     with self.vis():
@@ -60,14 +64,62 @@ class TestTask1(TestCase):
       self.assertEqual(x["abcdef"], 18, msg = "Read after store failed.")
 
     x["abdcef"] = 22
-    print(x.table)
+    with self.vis():
+      self.assertEqual(x["abdcef"], 22, msg = "Read after store failed.")
 
     assert self.check_okay("getitem")
 
-  # def test_rehash(self):
+  # Test for the set items
+  def test_setitems(self):
+    x = task1.HashTable(1024, 1)
 
-  #   assert self.check_okay("rehash")
+    # Set 2 values 
+    with self.vis("unexpected failure in setitem"):
+      x["abcdef"] = 18
+      x["definitely a string"] = None
+      
+    # Check wether 2 items are set properly or not
+    assert x.count == 2
 
+    # Check for each value
+    with self.vis():
+      self.assertEqual(x["abcdef"], 18, msg = "Read after store failed.")
+    with self.vis():
+      self.assertEqual(x["definitely a string"], None, msg = "Read after store failed.")
+
+    # Set one more item
+    x["abdcef"] = 22
+
+    assert x.count == 3
+    assert self.check_okay("setitem")
+
+  # Test rehash 
+  def test_rehash_1(self):
+    # This test case will test for different length
+    # Here length is 3 so new length should be grater then 2*old_length
+    # So here it should be 7
+
+    x = task1.HashTable(3,3)
+    x['abc'] = 1
+    x['abd'] = 2
+    x['abe'] = 3
+    x['abf'] = 4
+    
+    assert x.max_length == 7 
+    assert self.check_okay("rehash")
+
+  def test_rehash_2(self):
+    # This test case will test for different length
+    # Here length is 5 so new length should be grater then 2*old_length
+    # So here it should be 11
+    x = task1.HashTable(5,3)
+    x['abc'] = 1
+    x['abd'] = 2
+    x['abe'] = 3
+    x['abf'] = 4
+    x['abg'] = 5
+    assert x.max_length == 11 
+    assert self.check_okay("rehash")
 
 if __name__ == '__main__':
   unittest.main()
